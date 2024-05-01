fa0aaf9e79df4eab6e45b30c0ded05b1d5ec3a9a
Ayhem Hamdi <146385305+ayhem8hamdi@users.noreply.github.com>
2024-05-01 12:24:03 +0100

    Add files via upload

---
+++ impactCO2(1)/css/comp-script.js
@@ -0,0 +1,20 @@
+<div class="sections">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="section">
+                <h2>Comparer</h2>
+                <p>Comparer votre empreinte carbone à d'autres activités.</p>
+                <a href="#" class="bouton">Comparer</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="section">
+                <h2>Générer d'autres équivalents</h2>
+                <p>Découvrez l'impact CO2 d'autres actions quotidiennes.</p>
+                <a href="#" class="bouton" id="generateSections">Générer</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
